5ca8ede83518e2ef8720ae17929414adee80e29e
muh rifai <muhrifai554@gmail.com>
2022-05-11 19:04:59 +0700

    oke

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,22 @@ import cors from "cors";
 import express from "express";
 
 import { errorHandler } from "./middleware/errorHandler";
-import { getLanguage } from "./middleware/getLanguage";
-import routes from "./routes";
+// import { getLanguage } from "./middleware/getLanguage";
+// import routes from "./routes";
 // import { dbCreateConnection } from "./typeorm/dbCreateConnection";
 
 export const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(getLanguage);
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(getLanguage);
 
-app.use("/", routes);
+// app.use("/", routes);
 
 app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
+app.use("/", (req, res) => res.send(`oke ${port}`));
 app.listen(port, () => {
   console.log(`Server running  on port ${port}`);
 });
