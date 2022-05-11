314830f4a1758ebed9c805e2be48550c6d2f6c86
muh rifai <muhrifai554@gmail.com>
2022-05-11 08:59:17 +0700

    Heroku

--- src/typeorm/config/ormconfig.ts
+++ src/typeorm/config/ormconfig.ts
@@ -1,28 +1,41 @@
 import { ConnectionOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
-const config: ConnectionOptions = {
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  // host: process.env.PG_HOST,
-  // port: Number(process.env.PG_PORT),
-  // username: process.env.POSTGRES_USER,
-  // password: process.env.POSTGRES_PASSWORD,
-  // database: process.env.POSTGRES_DB,
-  synchronize: false,
-  logging: false,
-  entities: ["dist/typeorm/entities/**/*.js"],
-  // entities: ['src/typeorm/entities/**/*.ts'],
-  migrations: ["dist/typeorm/migrations/**/*.js"],
-  // migrations: ['src/typeorm/migrations/**/*.ts'],
-  // subscribers: ["disc/typeorm/subscriber/**/*.js"],
-  // subscribers: ["src/typeorm/subscriber/**/*.ts"],
-  cli: {
-    entitiesDir: "dist/typeorm/entities",
-    migrationsDir: "dist/typeorm/migrations",
-    subscribersDir: "dist/typeorm/subscriber",
+const config: any = {
+  production: {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    entities: ["dist/typeorm/entities/**/*.js"],
+    migrations: ["dist/typeorm/migrations/**/*.js"],
+    // subscribers: ["disc/typeorm/subscriber/**/*.js"],
+    cli: {
+      entitiesDir: "dist/typeorm/entities",
+      migrationsDir: "dist/typeorm/migrations",
+      subscribersDir: "dist/typeorm/subscriber",
+    },
+    namingStrategy: new SnakeNamingStrategy(),
   },
-  namingStrategy: new SnakeNamingStrategy(),
+  development: {
+    type: "postgres",
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: false,
+    logging: false,
+    entities: ["src/typeorm/entities/**/*.ts"],
+    migrations: ["src/typeorm/migrations/**/*.ts"],
+    subscribers: ["src/typeorm/subscriber/**/*.ts"],
+    cli: {
+      entitiesDir: "src/typeorm/entities",
+      migrationsDir: "src/typeorm/migrations",
+      subscribersDir: "src/typeorm/subscriber",
+    },
+    namingStrategy: new SnakeNamingStrategy(),
+  },
 };
 
 export = config;
--- src/typeorm/dbCreateConnection.ts
+++ src/typeorm/dbCreateConnection.ts
@@ -4,7 +4,12 @@ import config from "./config/ormconfig";
 
 export const dbCreateConnection = async (): Promise<Connection | null> => {
   try {
-    const conn = await createConnection(config);
+    const config_env =
+      process.env.NODE_ENV == "production"
+        ? config.production
+        : config.development;
+    console.log(process.env.NODE_ENV);
+    const conn = await createConnection(config_env);
     console.log(
       `Database connection success. Connection name: '${conn.name}' Database: '${conn.options.database}' ${process.env.DATABASE_URL}`
     );
